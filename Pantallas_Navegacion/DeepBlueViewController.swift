f9454e9738b294822ce97a659d29b4a173eb69f2
Alumno <ulsa@D109.local>
2022-09-21 08:45:22 -0700

    mensaje

---
+++ Pantallas_Navegacion/DeepBlueViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DeepBlueViewController.swift
+//  Pantallas_Navegacion
+//
+//  Created by Alumno on 21/09/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class DeepBlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Deep Blue screeen"
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pantallas_Navegacion/PantallaAmarillaController.swift
@@ -0,0 +1,13 @@
+//
+//  PantallaAmarillaController.swift
+//  Pantallas_Navegacion
+//
+//  Created by Alumno on 21/09/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class PantallaAmarillaController : UIViewController{
+    
+}
